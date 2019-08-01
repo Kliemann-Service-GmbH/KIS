9b94571994176cd75122c83ba478de00c29bb5f9
Stefan Müller zzeroo <co@zzeroo.com>
2019-08-01 16:00:13 +0200

    More german locales
    First german error message see 'app/models/gas_type.rb' and german routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,31 @@
 Rails.application.routes.draw do
   resources :addresses, only: [:index, :show]
+  resources :addresses, only: [:index, :show], path: "Adressen"
+
   resources :customers, only: [:index, :show]
+  resources :customers, path: "Kunden"
 
   resources :gas_types
+  resources :gas_types, path: "Gasarten"
+
   resources :sensor_types
+  resources :sensor_types, path: "Sensorarten"
+
   resources :si_units
+  resources :si_units, path: "SIEinheiten"
 
   resources :service_objects do
     resources :central_devices
   end
+  resources :service_objects, path: "Objekte"
 
   resources :central_devices do
     resources :service_protocols
   end
+  resources :central_devices, path: "Geraete"
 
-  # ServiceProtocols should only created via a present central_device
   resources :service_protocols
+  resources :service_protocols, path: "Pruefprotokolle"
 
   get 'welcome/index'
 
