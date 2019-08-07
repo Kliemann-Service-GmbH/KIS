04daee1d373f14dfd9e9dd71fb643015b6f61e32
Stefan Müller zzeroo <co@zzeroo.com>
2019-08-07 14:30:41 +0200

    Suche auf der Hauptseite verbessert
    Wird in das Suchfeld auf der Hauptseite http://kis kein Wert eingegben,
    und nur auf 'Suche' geklickt, werden alle suchbaren Dokumente und Daten
    gelistet.
    
    Dieses Verhalten funktioniert nun gleich wie bei all den anderen
    suchbaren Datentypen (Adressen, Protokolle, Objekte, ...)

--- test/system/welcome_test.rb
+++ test/system/welcome_test.rb
@@ -3,7 +3,19 @@ require "application_system_test_case"
 class WelcomeTest < ApplicationSystemTestCase
   setup do
     @address = addresses(:baroness)
+    @customer = customers(:complete)
+    @service_object = service_objects(:complete)
+    rebuild_multisearch
   end
+
+  # Recreate the pg_search multisearch index
+  def rebuild_multisearch
+    PgSearch::Document.delete_all()
+    [Customer, ServiceObject, CentralDevice, Address, ServiceProtocol].each do |klass|
+      PgSearch::Multisearch.rebuild(klass)
+    end
+  end
+
 
   test "visiting the index" do
     visit root_url
@@ -21,4 +33,27 @@ class WelcomeTest < ApplicationSystemTestCase
     # assert_selector "div", text: @address.address_details['last_name'] # failure
     assert_selector "div", text: @address.address_details['match_code']
   end
+
+  test "search field without value" do
+    visit root_url
+    # search
+    click_on I18n.t('Search')
+
+    assert_selector "div", text: @address.address_number
+    assert_selector "div", text: @customer.customer_number
+    assert_selector "div", text: @service_object.address.address_number
+  end
+
+  test "search Customer shows ServiceObjects" do
+    assert_equal @customer.address.address_number, @service_object.customer.address.address_number
+
+    visit root_url
+    # search
+    fill_in :q, with: @customer.address.address_number
+    click_on I18n.t('Search')
+
+    assert_selector "div", text: @customer.address.address_number
+    assert_selector "div", text: @service_object.address.address_number
+  end
+
 end
