d2e1ac9c6b06b2f619be45521c76a1f835775822
Stefan Müller zzeroo <co@zzeroo.com>
2019-10-16 09:36:43 +0200

    Datenbankabgleich in der Testumgebung deaktiviert
    Der Datenbankabgleich mit den Daten der EasyWinArt Datenbank wurde auch
    in der Testumgebung ausgeführt. Dies wurde behoben.

--- app/controllers/system_task_controller.rb
+++ app/controllers/system_task_controller.rb
@@ -1,8 +1,9 @@
 class SystemTaskController < ApplicationController
   def db_import_legacy
-    # call_rake db_import_legacy
-    call_rake 'db:import_legacy:customers'
-    flash[:notice] = t("Database synchronization started")
+    unless Rails.env.test?
+      call_rake 'db:import_legacy:customers'
+      flash[:notice] = t("Database synchronization started")
+    end
 
     # https://blog.bigbinary.com/2016/02/29/rails-5-improves-redirect_to_back-with-redirect-back.html
     redirect_back(fallback_location: root_path)
