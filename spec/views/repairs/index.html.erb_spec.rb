08172d1c7b956aca26f6ad37f3b383c7f36e8fa1
Stefan Müller zzeroo <co@zzeroo.com>
2020-02-05 13:09:32 +0100

    Tests lauffähig gemacht

--- spec/views/repairs/index.html.erb_spec.rb
+++ spec/views/repairs/index.html.erb_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "repairs/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders central_devices/_table_attributes for each central_device" do
+  end
 end
